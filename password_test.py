e7062d9b1569a56bed81850bd80031360921bcf8
Sun0506 <asd8061812200@gmail.com>
2019-07-15 21:37:01 +0800

    change to while times

--- password_test.py
+++ password_test.py
@@ -1,12 +1,10 @@
 user = 'a123456'
 times = 3
-while True:
+while times > 0:
 	password = input('請輸入密碼: ')
 	if password == user:
 		print('登入成功')
 		break
-	else:
+	else: 
 		times = times - 1
 		print('密碼錯誤! 還有',times,'次機會')
-		if times == 0:
-			break
